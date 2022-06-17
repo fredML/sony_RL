a41c15c92f044ab049d9cee01c9e715bdf4d67e6
fredML <dz_mode@hotmail.fr>
2022-06-17 15:13:34 +0000

    modification to add continuous carving but pb with intrinsic matrix calculation

--- base_functions/space_carving.py
+++ base_functions/space_carving.py
@@ -1,55 +1,70 @@
 import cv2
-from scipy.ndimage import rotate
-from cl import Backprojection
-from proc3d import vol2pcd_exp
 import numpy as np
 from skimage.morphology import binary_dilation
+from skimage.transform import resize
 import json
 from PIL import Image, ImageOps
 import glob
 import os
 from copy import deepcopy
+from utils import angle_to_position_continuous, get_mask, set_sc, make_extrinsics
 
 class space_carving_rotation_2d():
-    def __init__(self, model_path, phi_bias=0, total_phi_positions=180, cube_view='static', voxel_weights = None):
-        # if dynamic, perspective of the camera seeing the cube changes according to position
-        # if static, perspective is always the same
-        self.cube_view = cube_view
-        
-        # phi position bias for simulating rotation of position of the object
-        self.phi_bias = phi_bias
-        
-        # number of posible positions around the circle
-        self.total_phi_positions = total_phi_positions
-        
-        # get all .png file names of images from folder path
-        self.img_files = sorted (
-            glob.glob(os.path.join(model_path, 'imgs', '*.png')) )
-            
-        # get all .png file names of image masks from folder path
-        self.masks_files = sorted(
-            glob.glob(os.path.join(model_path, 'masks', '*.png')))
+    def __init__(self, model_path, total_phi_positions=None, voxel_weights=None, continuous=True):
 
-        self.gt_files = sorted(
-            glob.glob(os.path.join(model_path, 'ground_truth_volumes', '*.npy')))
-        
-        self.extrinsics = self.load_extrinsics(
-            os.path.join(model_path, 'extrinsics'))
+        self.camera_model = json.load(
+                open(os.path.join(model_path, 'old_camera_model.json')))
+
+        self.intrinsics = self.camera_model['params'][0:4]
         
         self.bbox = json.load(
             open(os.path.join(model_path, 'bbox_general.json')))
-   
-        self.camera_model = json.load(
-            open(os.path.join(model_path, 'camera_model.json')))
-        self.intrinsics = self.camera_model['params'][0:4]
 
         params = json.load(open(os.path.join(model_path, 'params.json')))
+
         self.n_dilation = params["sc"]["n_dilation"]
         self.voxel_size = params['sc']['voxel_size']
+        self.radius = params['traj']['R']
 
-        self.set_sc(self.bbox)
+        self.origin, self.sc, self.last_volume = set_sc(self.bbox, self.voxel_size)
         self.vol_shape = self.sc.values().shape
-        self.voxel_weights = voxel_weights
+
+        if voxel_weights is not None:
+            self.voxel_weights = voxel_weights
+        else:
+            self.voxel_weights = np.ones(self.vol_shape)
+
+        if continuous:
+            path = os.getcwd()
+            os.chdir('/mnt/diskSustainability/frederic/rlviewer')
+            os.environ['DISPLAY'] = ':1'
+            import rlviewer
+            os.chdir(path)
+
+            rlviewer.load(os.path.join(model_path,'sphere.obj')) 
+            rlviewer.set_light(0, 120, 0, 0, 5000) 
+            rlviewer.set_light(1, -120, 0, 0, 5000) 
+            rlviewer.set_light(2, 0, 0, 120, 5000) 
+            rlviewer.set_light(3, 0, 0, -120, 5000) 
+        
+        else:
+            self.total_phi_positions = total_phi_positions
+
+            self.img_files = sorted (
+                glob.glob(os.path.join(model_path, 'imgs', '*.png')) )
+        
+            self.masks_files = sorted(
+                glob.glob(os.path.join(model_path, 'masks', '*.png')))
+
+            self.gt_files = sorted(
+                glob.glob(os.path.join(model_path, 'ground_truth_volumes', '*.npy')))
+            
+            self.extrinsics = self.load_extrinsics(
+                os.path.join(model_path, 'extrinsics'))
+
+            self.gt = np.load(self.gt_files[0])
+            self.gt_solid_mask = np.where(self.gt == 1, True, False)
+            self.gt_n_solid_voxels = np.sum(self.gt_solid_mask*self.voxel_weights)
 
         ## reweighting of the voxels
 
@@ -84,14 +99,9 @@ class space_carving_rotation_2d():
         self.voxel_weights = distance_transform_edt(self.index)
         self.voxel_weights = 1-self.voxel_weights/np.max(self.voxel_weights)'''
 
-        # uses ground truth model 
-        self.gt = np.load(self.gt_files[phi_bias])
-        self.gt_solid_mask = np.where(self.gt == 1, True, False)
-        self.gt_n_solid_voxels = np.sum(self.gt_solid_mask*self.voxel_weights)
-
     def reset(self):
         del(self.sc)
-        self.set_sc(self.bbox)
+        self.origin, self.sc, self.last_volume = set_sc(self.bbox, self.voxel_size)
 
     def load_extrinsics(self, path):
         ext = []
@@ -106,13 +116,8 @@ class space_carving_rotation_2d():
         self.mask = img
         return img
     
-    def get_pcd(self):
-        return vol2pcd_exp(self.last_volume, self.origin, self.voxel_size, level_set_value=0)
-    
     def get_image(self, phi, theta):
-        biased_phi = self.calculate_phi_position(phi, -self.phi_bias)
-        image_idx = (self.total_phi_positions * theta) + biased_phi
-        
+        image_idx = (self.total_phi_positions * theta) + phi
         img = Image.open(self.img_files[image_idx])
         cp = img.copy()
         cp = cp.resize((256,256))
@@ -120,54 +125,39 @@ class space_carving_rotation_2d():
         img.close()
 
         return cp
-    
-    def set_sc(self, bbox):
-        x_min, x_max = bbox['x']
-        y_min, y_max = bbox['y']
-        z_min, z_max = bbox['z']
-
-        nx = int((x_max - x_min) / self.voxel_size) + 1
-        ny = int((y_max - y_min) / self.voxel_size) + 1
-        nz = int((z_max - z_min) / self.voxel_size) + 1
-
-        self.origin = np.array([x_min, y_min, z_min])
-        self.sc = Backprojection(
-            [nx, ny, nz], [x_min, y_min, z_min], self.voxel_size)
-        self.last_volume = deepcopy(self.sc.values())
 
     def carve(self, phi, theta):
         '''space carve in position theta(rows),phi(cols)
         theta and phi are steps of the scanner, not angles'''
-        # if using phi bias, use image of biased (shifted)phi with extrinsics
-        #of current position to create a rotate model
-        biased_phi = self.calculate_phi_position(phi, -self.phi_bias)
-        image_idx = (self.total_phi_positions * theta) + biased_phi
-        
-        extrinsics_idx = (self.total_phi_positions * theta) + phi
-        
-        mask = self.load_mask(image_idx)
-        self.space_carve(mask, self.extrinsics[extrinsics_idx])
+   
+        idx = (self.total_phi_positions * theta) + phi
+        mask = self.load_mask(idx)
+        self.space_carve(mask, self.extrinsics[idx])
 
         volume = self.sc.values()
-        
-        if self.cube_view == 'dynamic':
-            # rotate cube according to current camera position
-            #moves current position's perspective to cube position 0 so position 0
-            #in cube always shows current position's perspective
-            self.volume = rotate(self.volume,
-                            angle=-phi*(360//self.total_phi_positions),reshape=False)
-
         self.carved_voxels = ((volume == -1) & (self.last_volume != -1))*self.voxel_weights
         self.last_volume = deepcopy(volume)
 
+    def continuous_carve(self, phi, theta):
+        img = rlviewer.grab(self.radius, theta-np.pi/2, phi) 
+        img = resize(img, (256,256), preserve_range=True)
+        self.img = img
+        mask = get_mask(img)
+        extrinsics = make_extrinsics(self.radius, angle_to_position_continuous(phi,theta))
+        self.extrinsics = extrinsics
+        self.space_carve(mask, self.extrinsics)
+
+        volume = self.sc.values()
+        self.carved_voxels = ((volume == -1) & (self.last_volume != -1))*self.voxel_weights
+        self.last_volume = deepcopy(volume)
+        
     def space_carve(self, mask, rt):
         '''do space carving on mask with preset parameters'''
-        # mask = im.copy() #mask = get_mask(im)
         rot = sum(rt['R'], [])
         tvec = rt['T']
-        if self.n_dilation:
+        '''if self.n_dilation:
             for _ in range(self.n_dilation):
-                mask = binary_dilation(mask)
+                mask = binary_dilation(mask)'''
         self.sc.process_view(self.intrinsics, rot, tvec, mask)
 
     def gt_compare(self):
@@ -195,12 +185,3 @@ class space_carving_rotation_2d():
         
         ratio = n_intersection / (self.gt_n_solid_voxels + vol_n_solid_voxels - n_intersection)
         return ratio
-
-    def calculate_phi_position(self, init_state, steps):
-        '''calculates phi position (in steps) from current phi plus n steps'''
-        n_pos = init_state + steps
-        if n_pos > (self.total_phi_positions-1):
-            n_pos -= self.total_phi_positions
-        elif n_pos < 0:
-            n_pos += self.total_phi_positions
-        return n_pos
--- base_functions/utils.py
+++ base_functions/utils.py
@@ -1,20 +1,16 @@
 from scipy.spatial import cKDTree as KDTree
 import numpy as np
 import cv2
-import chex 
-import jax
 import json
 import urllib
 import cl
+from copy import deepcopy
 
 def chamfer_d(tree1, pc_1, pc_2):
-    #tree = KDtree(pc_1)
-    #ds, _ = tree.query(pc_2)
     ds, _ = tree1.query(pc_2, workers=-1)
     d_21 = np.mean(ds)
 
     tree2 = KDTree(pc_2)
-    #ds, _ = tree.query(pc_1)
     ds, _ = tree2.query(pc_1, workers=-1)
     d_12 = np.mean(ds)
     return d_21 + d_12
@@ -41,9 +37,8 @@ def get_frame(img):
     small = cv2.resize(res_bg, (84, 84))
     return small
 
-def set_sc(scanner,voxel_size):
+def set_sc(bbox,voxel_size):
 
-  bbox = json.loads(urllib.request.urlopen(scanner.localhost + 'bounding_box').read().decode('utf-8'))
   x_min, x_max = bbox['x']
   y_min, y_max = bbox['y']
   z_min, z_max = bbox['z']
@@ -53,7 +48,9 @@ def set_sc(scanner,voxel_size):
   nz = int((z_max - z_min) / voxel_size) + 1
 
   origin = np.array([x_min, y_min, z_min])
-  return origin, cl.Backprojection([nx, ny, nz], [x_min, y_min, z_min], voxel_size), bbox
+  sc = cl.Backprojection([nx, ny, nz], [x_min, y_min, z_min], voxel_size)
+  last_volume = deepcopy(sc.values())
+  return origin, sc, last_volume
 
 def get_intrinsics(scanner):
     url_part = 'camera_intrinsics'
@@ -61,25 +58,25 @@ def get_intrinsics(scanner):
         scanner.localhost + url_part).read().decode('utf-8'))
     return camera_model['params'][0:4]
 
-def val(features,i):
-  return features[i] 
+def make_extrinsics(radius, pos):
+    pos = pos/np.linalg.norm(pos)
+    n = len(pos)
+    up = np.array([0,0,1])
+    R = np.zeros((n,n))
+    R[2] = -pos
+    R[0] = np.cross(up,pos)
+    R[1] = np.cross(R[2],R[0])
 
-def batched_indexing(features: chex.Array,idxs: chex.Array) -> chex.Array:
-  return jax.vmap(val)(features,idxs)
+    T = np.array([0,0, radius])
 
-def run_test(env, network_apply, online_params):
-    chamfer_d = []
-    positions = []
+    return {'R':R.tolist(), 'T':T.tolist()}
 
-    ts = env.reset(theta=np.pi/2,phi=0)
-    chamfer_d.append(env.chamfer_d())
-    positions.append([env.x, env.y, env.z])
+def angle_to_position(phi, theta):
+    return np.array([np.cos(2*phi*np.pi/180)*np.sin((theta+1)*np.pi/8),
+                    np.sin(2*phi*np.pi/180)*np.sin((theta+1)*np.pi/8),
+                    np.cos((theta+1)*np.pi/8)])
 
-    for k in range (19):
-        q = network_apply(online_params, ts.observation[None])[0]
-        a = np.argmax(q)
-        ts = env.step(a)
-        chamfer_d.append(env.chamfer_d())
-        positions.append([env.x, env.y, env.z])
-
-    return chamfer_d, positions
+def angle_to_position_continuous(phi, theta):
+    return np.array([np.cos(phi)*np.sin(theta),
+                     np.sin(phi)*np.sin(theta),
+                     np.cos(theta)])
