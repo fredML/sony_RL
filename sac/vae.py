aefa1ff5ef809cb84f315b309e9af4962da0d9a8
fredML <dz_mode@hotmail.fr>
2022-07-18 13:50:22 +0000

    fix class names

--- sac/vae.py
+++ sac/vae.py
@@ -29,7 +29,7 @@ class UpBlock(hk.Module):
 
         return s
 
-class UpBlock_2(hk.Module):
+class UpBlockNoBN(hk.Module):
     def __init__(self, filter_size):
         super().__init__()
         self.filter_size = filter_size
@@ -42,14 +42,18 @@ class UpBlock_2(hk.Module):
         return s
 
 class Encoder(hk.Module):
-    def __init__(self, filter_sizes):
+    def __init__(self, latent_dim, filter_sizes):
         super().__init__()
         self.filter_sizes = filter_sizes
+        self.latent_dim = latent_dim
     
     def __call__(self, s, is_training):
         for filter_size in self.filter_sizes:
             s = DownBlock(filter_size)(s, is_training)
-        return s
+        s = hk.Flatten()(s)
+        mu = hk.Linear(self.latent_dim)(s)
+        log_var = hk.Linear(self.latent_dim)(s)
+        return mu, log_var
 
 class Decoder(hk.Module):
     def __init__(self, last_conv_shape, filter_sizes):
@@ -67,41 +71,37 @@ class Decoder(hk.Module):
         s = hk.Conv2D(3, kernel_shape=3, padding='SAME')(s)
         return jax.nn.sigmoid(s)
 
-class Decoder_2(hk.Module):
+class DecoderNoBN(hk.Module):
     def __init__(self, filter_sizes):
         super().__init__()
         self.filter_sizes = filter_sizes
     
     def __call__(self, s):
         for filter_size in self.filter_sizes:
-            s = UpBlock_2(filter_size)(s)
+            s = UpBlockNoBN(filter_size)(s)
         s = hk.Conv2D(3, kernel_shape=3, padding='SAME')(s)
-        return jax.nn.sigmoid(s)
+        return s
 
 class VAE(hk.Module): 
 
     def __init__(self, input_size, latent_dim, filter_sizes):
         super().__init__()
-        self.latent_dim = latent_dim
-        self.encoder = Encoder(filter_sizes)
+        self.encoder = Encoder(latent_dim, filter_sizes)
         n = len(filter_sizes)
         last_conv_shape = input_size//2**n
         self.decoder = Decoder(last_conv_shape, filter_sizes[::-1])
 
     def __call__(self, s, is_training):
-        enc_output = self.encoder(s, is_training)
-        h = hk.Flatten()(enc_output)
-        mu = hk.Linear(self.latent_dim)(h)
-        log_var = hk.Linear(self.latent_dim)(h)
+        mu, log_var = self.encoder(s, is_training)
         sigma = jnp.exp(0.5*log_var)*is_training
         latent = mu + np.random.normal(0, 1, size=sigma.shape)*sigma
 
-        recons = self.decoder(h, is_training)
+        recons = self.decoder(latent, is_training)
 
         return recons, latent, mu, log_var
 
 class ResNetVAE(hk.Module):
-    def __init__(self, latent_dim, filter_sizes):
+    def __init__(self, latent_dim, filter_sizes, weights, pooling):
         import os
         path = os.getcwd()
         os.chdir('/mnt/diskSustainability/frederic/haikumodels') 
@@ -110,8 +110,8 @@ class ResNetVAE(hk.Module):
 
         super().__init__()
         self.latent_dim = latent_dim
-        self.encoder = hm.ResNet50(include_top=False)
-        self.decoder = Decoder_2(filter_sizes[::-1])
+        self.encoder = hm.ResNet50(include_top=False, weights=weights, pooling=pooling)
+        self.decoder = DecoderNoBN(filter_sizes[::-1])
         self.enc_shape = (1,4,4,2048)
         self.enc_flattened_shape = 4*4*2048
     
@@ -120,15 +120,18 @@ class ResNetVAE(hk.Module):
             enc_output = self.encoder(s, encoder_bn_training)
         else:
             enc_output = jax.lax.stop_gradient(self.encoder(s, encoder_bn_training))
+
         h = hk.Flatten()(enc_output)
-        latent = hk.Linear(self.latent_dim)(h)
+        mu = hk.Linear(self.latent_dim)(h)
+        log_var = hk.Linear(self.latent_dim)(h)
+        sigma = jnp.exp(0.5*log_var)*encoder_training
+        latent = mu + np.random.normal(0, 1, size=sigma.shape)*sigma
         h = jax.nn.leaky_relu(latent, 0.01)
         h = hk.Linear(self.enc_flattened_shape)(h)
         h = h.reshape(-1, 4, 4, 2048)
         recons = self.decoder(h)
-        return recons, latent
+        return recons, latent, mu, log_var
 
 def kl_gaussian(mean: jnp.ndarray, log_var: jnp.ndarray) -> jnp.ndarray:
 
   return 0.5 * jnp.sum(-log_var - 1.0 + jnp.exp(log_var) + jnp.square(mean), axis=-1).mean()
-
