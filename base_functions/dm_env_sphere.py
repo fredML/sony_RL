f90478bc65d365b01d92f280d56b46dbcf455501
fredML <dz_mode@hotmail.fr>
2022-07-20 15:39:39 +0000

    add segmentation masks

--- base_functions/dm_env_sphere.py
+++ base_functions/dm_env_sphere.py
@@ -3,12 +3,10 @@ from space_carving import *
 import dm_env
 from acme import specs
 from utils import angle_to_position_continuous
-from skimage.transform import resize
-
 
 class SphereEnv(dm_env.Environment):
 
-    def __init__(self, objects_path, env_shape, img_shape, voxel_weights=None, rmax=0.9, k_d=0, k_t=0, apply_ae=None, params_ae=None):
+    def __init__(self, objects_path, img_shape, voxel_weights=None, rmax=0.9, k_d=0, k_t=0, apply_ae=None, params_ae=None):
 
         super(SphereEnv, self).__init__()
         self.objects_path = objects_path
@@ -20,13 +18,27 @@ class SphereEnv(dm_env.Environment):
         self.k_d = k_d #penalty based on distance between two camera positions
         self.k_t = k_t #penalty based on number of views
 
-        self.opt_pos = angle_to_position_continuous(np.pi*np.array([1/2,1/4,1/4]), np.pi*np.array([0,1/2,-1/2]))
+        self.sphere_radius = 5
+        self.opt_theta = np.pi*np.array([1/2,1/4,3/4,1/2,1/4,3/4])
+        self.opt_phi = np.pi*np.array([0,1/2,1/2,1,-1/2,-1/2])
+        self.opt_pos = self.sphere_radius * angle_to_position_continuous(self.opt_theta, self.opt_phi).T
 
-        self.env_shape = env_shape
         self.img_shape = img_shape # for reshaping images
         self.apply_ae = apply_ae
         if params_ae is not None:
             self.params_ae, self.bn_ae_state = params_ae
+
+        neigh_xyz = []
+        dtheta = 6*np.pi/180
+        dphi = 6*np.pi/180
+        for i in range(len(self.opt_theta)):
+            theta, phi = self.opt_theta[i], self.opt_phi[i]
+            for val_theta in np.linspace(theta-dtheta,theta+dtheta,50):
+                for val_phi in np.linspace(phi-dphi,phi+dphi,50):
+                    neigh_xyz.append(self.sphere_radius*angle_to_position_continuous(val_theta, val_phi))
+        
+        neigh_xyz = np.array(neigh_xyz)
+        self.neigh_xyz = neigh_xyz
 
     def reset(self, theta_init=-1, phi_init=-1) -> dm_env.TimeStep:
         self.num_steps = 0
@@ -57,7 +69,7 @@ class SphereEnv(dm_env.Environment):
         # create space carving objects
         self.spc.reset()
 
-        pos = angle_to_position_continuous(
+        pos = self.spc.radius*angle_to_position_continuous(
             self.current_theta, self.current_phi)
         self.pos = pos
         img = self.spc.get_image_continuous(self.current_theta, self.current_phi)
@@ -71,18 +83,21 @@ class SphereEnv(dm_env.Environment):
 
         if self.apply_ae is not None:
             if self.bn_ae_state is not None:
-                reconst_latent, _ = self.apply_ae(self.params_ae, self.bn_ae_state, self.img[None], False)
-                reconst, latent = reconst_latent
+                vae_output, _ = self.apply_ae(self.params_ae, self.bn_ae_state, self.img[None], False)
+                '''latent = np.array(vae_output['vq_output']['quantize'][0]) 
+                latent = latent.mean(axis=(0,1))'''
+                latent = vae_output[1][0]
                 
             else:
                 _, latent = self.apply_ae(self.params_ae, self.img[None])
-
-            latent = latent[0]   
+  
             self.observation = np.concatenate((latent, latent, latent))
         
         else:
             self.observation = self.last_k_img
             #self.observation = self.last_k_pos.astype('float32')
+
+        self.observation_shape = self.observation.shape
 
         return dm_env.restart(self.observation)
 
@@ -90,10 +105,10 @@ class SphereEnv(dm_env.Environment):
         theta, phi = action
         self.num_steps += 1
         self.current_theta += theta
-        if self.current_theta > np.pi/2:
-            self.current_theta -= np.pi/2
+        if self.current_theta > np.pi:
+            self.current_theta -= np.pi
         elif self.current_theta < 0:
-            self.current_theta += np.pi/2
+            self.current_theta += np.pi
 
         self.current_phi += phi
         if self.current_phi >= 2*np.pi:
@@ -107,8 +122,9 @@ class SphereEnv(dm_env.Environment):
 
         self.reward = self.spc.gt_compare()
 
-        pos = angle_to_position_continuous(
+        pos = self.spc.radius*angle_to_position_continuous(
             self.current_theta, self.current_phi)
+        self.pos = pos
         img = self.spc.img
         pil_img = Image.fromarray(img).resize((self.img_shape, self.img_shape))
         self.img = np.array(pil_img)/255
@@ -128,14 +144,15 @@ class SphereEnv(dm_env.Environment):
 
         if self.apply_ae is not None:
             if self.bn_ae_state is not None:
-                reconst_latent, _ = self.apply_ae(self.params_ae, self.bn_ae_state, self.img[None], False)
-                reconst, latent = reconst_latent
+                vae_output, _ = self.apply_ae(self.params_ae, self.bn_ae_state, self.img[None], False)
+                '''latent = np.array(vae_output['vq_output']['quantize'][0]) 
+                latent = latent.mean(axis=(0,1))'''
+                latent = vae_output[1][0]
                 
             else:
                 _, latent = self.apply_ae(self.params_ae, self.img[None])
 
-            latent = latent[0]               
-            self.observation = np.concatenate((self.observation[3:], latent))
+            self.observation = np.concatenate((self.observation[len(latent):], latent))
         
         else:
             self.observation = self.last_k_img
@@ -152,19 +169,30 @@ class SphereEnv(dm_env.Environment):
         self.current_theta = theta
         self.current_phi = phi
 
+        if self.current_theta > np.pi/2:
+            self.current_theta -= np.pi/2
+        elif self.current_theta < 0:
+            self.current_theta += np.pi/2
+
+        if self.current_phi >= 2*np.pi:
+            self.current_phi -= 2*np.pi
+        elif self.current_phi < 0:
+            self.current_phi += 2*np.pi
+
         self.visited_positions.append([self.current_theta, self.current_phi])
 
         self.spc.continuous_carve(self.current_theta, self.current_phi)
 
         self.reward = self.spc.gt_compare()
 
-        pos = angle_to_position_continuous(
+        pos = self.spc.radius*angle_to_position_continuous(
             self.current_theta, self.current_phi)
+        self.pos = pos
         img = self.spc.img
         pil_img = Image.fromarray(img).resize((self.img_shape, self.img_shape))
         self.img = np.array(pil_img)/255
         img_gray = np.array(pil_img.convert('L'))/255
-        self.penalty = np.linalg.norm(pos-self.last_k_pos[-3:])
+        self.penalty = np.linalg.norm(pos - self.last_k_pos[-3:])
         self.last_k_pos = np.concatenate((self.last_k_pos[3:], pos))
         self.last_k_img = np.concatenate((self.last_k_img[...,1:], img_gray[...,None]), axis=-1)
 
@@ -178,15 +206,15 @@ class SphereEnv(dm_env.Environment):
 
         if self.apply_ae is not None:
             if self.bn_ae_state is not None:
-                reconst_latent, _ = self.apply_ae(self.params_ae, self.bn_ae_state, self.img[None], False)
-                reconst, latent = reconst_latent
+                vae_output, _ = self.apply_ae(self.params_ae, self.bn_ae_state, self.img[None], False)
+                '''latent = np.array(vae_output['vq_output']['quantize'][0]) 
+                latent = latent.mean(axis=(0,1))'''
+                latent = vae_output[1][0]
                 
             else:
                 _, latent = self.apply_ae(self.params_ae, self.img[None])
-
-            latent = latent[0]    
             
-            self.observation = np.concatenate((self.observation[3:], latent))
+            self.observation = np.concatenate((self.observation[len(latent):], latent))
         
         else:
             self.observation = self.last_k_img
@@ -197,10 +225,46 @@ class SphereEnv(dm_env.Environment):
 
         return dm_env.transition(reward=self.reward*1., observation=self.observation)
 
+
+    def segmentation_mask(self):
+
+        segmentation_mask = np.zeros((1024,768))
+
+        distances = np.linalg.norm(self.pos - self.neigh_xyz, axis=1)
+        self.distances = distances
+        mask = np.where(distances < np.sqrt(self.spc.radius**2 + self.sphere_radius**2))
+        self.neigh_xyz = self.neigh_xyz[mask]
+
+        extrinsics = self.spc.extrinsics
+        R = np.array(extrinsics['R'])
+        T = np.array(extrinsics['T'])
+
+        pixels = R.dot(self.neigh_xyz.T) + T[...,None] #transform world coordinates to camera coordinates
+        mask = np.where(pixels[2]>0)
+        pixels = pixels[:,mask[0]]
+
+        K = self.spc.intrinsics
+        x = K[0]*pixels[0]/pixels[2] + K[2]
+        y = K[1]*pixels[1]/pixels[2] + K[3]
+        x = x.astype('int32')
+        y = y.astype('int32')
+
+        self.pixels = (x,y)
+
+        for i in range (len(x)):
+            segmentation_mask[x[i],y[i]] = 1
+        
+        segmentation_mask = segmentation_mask.T
+        pil_img = Image.fromarray(segmentation_mask).resize((self.img_shape, self.img_shape))
+        return np.array(pil_img)
+
+
     def observation_spec(self) -> specs.BoundedArray:
-        if (len(self.env_shape) == 3) & (self.apply_ae is None):
+        '''if (len(self.env_shape) == 3) & (self.apply_ae is None):
             return specs.BoundedArray(shape=self.env_shape, minimum=0, maximum=255, dtype=np.uint8)
-        return specs.BoundedArray(shape=self.env_shape, minimum=-1, maximum=1, dtype=np.float32)
+        return specs.BoundedArray(shape=self.env_shape, minimum=-1, maximum=1, dtype=np.float32)'''
+        pass
 
     def action_spec(self) -> specs.BoundedArray:
-        return specs.BoundedArray(shape=(2,), minimum=-1, maximum=1, dtype=np.float32)
+        '''return specs.BoundedArray(shape=(2,), minimum=-1, maximum=1, dtype=np.float32)'''
+        pass
